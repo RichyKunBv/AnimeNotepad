2544ca737f790b8970d985a6f5f2032632eb5b11
RichyKunBv <reepito03@gmail.com>
2026-07-05 17:59:58 -0600

    solucion de micro errores

--- src/Verzion.cs
+++ src/Verzion.cs
@@ -8,6 +8,6 @@ namespace AnimeNotepad
 {
     public static class Verzion
     {
-        public static string Texto { get; set; } = "V2.0.0";
+        public static string Texto { get; set; } = "V2.0.1";
     }
 }
